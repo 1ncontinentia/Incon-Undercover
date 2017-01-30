bcace0be518241a058366348d6369f7a6261b6e3
1ncontinentia <jaemmerson@gmail.com>
2017-01-30 23:04:46 +0000

    Fixed small error with missing ; in RPT

--- INC_undercover/cfgFunctions.hpp
+++ INC_undercover/cfgFunctions.hpp
@@ -6,10 +6,10 @@ class INC_undercover
 	{
 		file = "INC_undercover\func";
 		class addEH {description = "Handles enemy deaths including suspecting nearby undercover units and reprisals against civilians.";};
-		class armedLoop  {description = "Contains functions for arming recruitable civilians."};
+		class armedLoop  {description = "Contains functions for arming recruitable civilians.";};
 		class compromised {description = "Sets the unit as compromised while it is know to enemy units and is doing something naughty.";};
 		class cooldown {description = "Initiates a cooldown after the unit has done something naughty";};
-		class gearHandler {description = "Contains functions for gear checks and actions."};
+		class gearHandler {description = "Contains functions for gear checks and actions.";};
 		class getConfigInfo {description = "Gets config information on a given faction / unit.";};
 		class initUcrVars {description = "Sets variables for the mission based on setup.sqf.";};
 		class isKnownToSide {description = "Returns whether there are alive groups of the given side who know about the unit.";};
@@ -17,6 +17,6 @@ class INC_undercover
 		class recruitHandler {description = "Handles all civilian recruitment.";};
 		class suspiciousEny {description = "Suspicious enemy behaviour.";};
 		class UCRhandler {description = "Gets detection scripts running on unit.";};
-		class ucrMain {description = "Contains primary UCR functions."};
+		class ucrMain {description = "Contains primary UCR functions.";};
 	};
 };
